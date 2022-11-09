55a1bfe22c5f714cb2d2f1eb09e4d6dd50ea7ae0
Shomir Uddin <shomiruddin@shomirs-mbp.myfiosgateway.com>
2022-11-08 22:01:56 -0500

    created framework

---
+++ src/main/java/com/saucedemo/utils/FrameworkConstants.java
@@ -0,0 +1,10 @@
+package com.saucedemo.utils;
+
+public class FrameworkConstants {
+
+    public static final String CONFIGURATION_FILEPATH = System.getProperty("user.dir")
+            + "/src/main/java/com/chroma/resources/config/localEnv.properties";
+
+    public static final String GET_OS_NAME = System.getProperty("os.name");
+
+}
