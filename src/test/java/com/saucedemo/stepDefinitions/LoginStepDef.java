6ad9c22b9108bb8652b715f3192df243fe3b9704
ulyanamazur <113566213+ulyanamazur@users.noreply.github.com>
2022-11-15 08:47:35 -0500

    Revert "added stepdef"

--- src/test/java/com/saucedemo/stepDefinitions/LoginStepDef.java
+++ src/test/java/com/saucedemo/stepDefinitions/LoginStepDef.java
@@ -1,39 +1,5 @@
 package com.saucedemo.stepDefinitions;
 
-import com.saucedemo.appsCommon.PageInitializer;
-import com.saucedemo.utils.ConfigReader;
-import com.saucedemo.web.WebDriverUtils;
-
-import cucumber.api.java.en.Given;
-import cucumber.api.java.en.Then;
-import cucumber.api.java.en.When;
-
-public class LoginStepDef extends PageInitializer {
-
-    @Given("user is on saucedemo homepage")
-    public void user_is_on_saucedemo_homepage() {
-        WebDriverUtils.driver.get(ConfigReader.getPropertyValue("url"));
-
-    }
-
-    @When("user enters username {string}")
-    public void user_enters_username(String string) {
-        loginPage.getLoginBtn().sendKeys(string);
-    }
-
-    @When("user enters password  {string}")
-    public void user_enters_password(String string) {
-        loginPage.getPasswordTextbox().sendKeys(string);
-    }
-
-    @When("user clicks login")
-    public void user_clicks_login() {
-        loginPage.getLoginBtn().click();
-    }
-
-    @Then("user is logged in")
-    public void user_is_logged_in() {
-        
-    }
-
+public class LoginStepDef {
+    
 }
